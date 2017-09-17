e4d95917245f0681a92e8ad9f4291b058d6d9bff
Ethan Chen <intervigil@gmail.com>
2017-09-16 22:14:36 -0700

    msm8916-common: Add basic USB HAL that reports no status change
    Change-Id: I38bfe869f9b1b66db0e13249f65e438878d06eff
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -206,7 +206,7 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.cyanogen_8916
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp,adb \
