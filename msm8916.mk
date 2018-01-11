0d3caa846dd8b6a0b7c33e2bd2ced0d6ee9c01fd
Vachounet <vachounet@live.fr>
2018-01-11 13:57:17 +0100

    msm8916-common: Build configstore HIDL service
     * Needed for triple framebuffer

--- msm8916.mk
+++ msm8916.mk
@@ -76,6 +76,7 @@ PRODUCT_PACKAGES += \
     memtrack.msm8916
 
 PRODUCT_PACKAGES += \
+    android.hardware.configstore@1.0-service \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
